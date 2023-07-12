2596a8dbd5b4bf3ce2a151392a25624177a153eb
Rui Machado Soares De Almeida <rumachad@c1r10s2.42porto.com>
2023-07-12 15:29:38 +0100

    so_long

--- so_long/so_long.c
+++ so_long/so_long.c
@@ -6,23 +6,41 @@
 /*   By: rumachad <rumachad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 10:23:12 by rumachad          #+#    #+#             */
-/*   Updated: 2023/07/11 13:13:25 by rumachad         ###   ########.fr       */
+/*   Updated: 2023/07/12 15:27:28 by rumachad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int	main()
+typedef struct s_mlx
 {
-    void    *mlx_ptr;
-    void    *mlx_win;
-	void	*img;
-	int x;
-	int y;
-    
-    mlx_ptr = mlx_init();
-    mlx_win = mlx_new_window(mlx_ptr, 800, 600, "so_long");
-	img = mlx_xpm_file_to_image(mlx_ptr, "./test.xpm", &x, &y);
-    mlx_loop(mlx_ptr);
-    return (0);
+	void *mlx_ptr;
+	void *win_ptr;
+}				t_mlx;
+
+int key(int keyp, t_mlx *vars)
+{
+	if (keyp == 65307)
+	{
+		mlx_destroy_window(vars->mlx_ptr, vars->win_ptr);
+		mlx_loop_end(vars->mlx_ptr);
+	}
+	else
+	{
+		printf("%d\n", keyp);
+		mlx_hook(vars->mlx_ptr, 7, (1L<<4), key, &vars);
+	}
+	return (0);
+}
+
+int main()
+{
+	t_mlx	vars;
+
+	vars.mlx_ptr = mlx_init();
+	vars.win_ptr = mlx_new_window(vars.mlx_ptr, 800, 600, "so_long");
+	mlx_key_hook(vars.win_ptr, key, &vars);
+	mlx_loop(vars.mlx_ptr);
+	mlx_destroy_display(vars.mlx_ptr);
+	return (0);
 }
