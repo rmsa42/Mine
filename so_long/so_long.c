0ce9ae1b44083fe6b8fe451db4bb987eced1b143
Rui Machado Soares De Almeida <rumachad@c1r10s2.42porto.com>
2023-07-13 16:59:44 +0100

    So_Long

--- so_long/so_long.c
+++ so_long/so_long.c
@@ -6,11 +6,14 @@
 /*   By: rumachad <rumachad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 10:23:12 by rumachad          #+#    #+#             */
-/*   Updated: 2023/07/12 15:27:28 by rumachad         ###   ########.fr       */
+/*   Updated: 2023/07/13 16:56:21 by rumachad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+
+int	width = 1281;
+int	height = 721;
 
 typedef struct s_mlx
 {
@@ -18,29 +21,41 @@ typedef struct s_mlx
 	void *win_ptr;
 }				t_mlx;
 
-int key(int keyp, t_mlx *vars)
+typedef struct s_image
 {
-	if (keyp == 65307)
+	int x;
+	int y;
+	char *pixel;
+	void *image;
+	int *size_line;
+	int *bits_p;
+	int *endian;
+}				t_image;
+
+int key_press(int key, t_mlx *vars)
+{
+	if (key == 65307)
 	{
 		mlx_destroy_window(vars->mlx_ptr, vars->win_ptr);
-		mlx_loop_end(vars->mlx_ptr);
+		exit(0);
 	}
-	else
-	{
-		printf("%d\n", keyp);
-		mlx_hook(vars->mlx_ptr, 7, (1L<<4), key, &vars);
-	}
+	printf("%d\n", key);
 	return (0);
 }
 
 int main()
 {
 	t_mlx	vars;
-
+	t_image im;
+	
 	vars.mlx_ptr = mlx_init();
-	vars.win_ptr = mlx_new_window(vars.mlx_ptr, 800, 600, "so_long");
-	mlx_key_hook(vars.win_ptr, key, &vars);
+	vars.win_ptr = mlx_new_window(vars.mlx_ptr, width, height, "so_long");
+	mlx_key_hook(vars.win_ptr, key_press, &vars);
+	
+	im.image = mlx_xpm_file_to_image(vars.mlx_ptr, "test.xpm", &im.x, &im.y);
+	im.pixel = mlx_get_data_addr(im.image, im.bits_p, im.size_line, im.endian);
+	mlx_put_image_to_window(vars.mlx_ptr, vars.win_ptr, im.image, 0, 0);
+	
 	mlx_loop(vars.mlx_ptr);
-	mlx_destroy_display(vars.mlx_ptr);
 	return (0);
 }
